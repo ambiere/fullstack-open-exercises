ce81c67b900791d1e93fb1ddcd885f9a3748c553
zhid0399123 <zhid0399123@gmail.com>
2023-08-05 14:15:48 +0300

    refactor(anecdotes): removed typescript and converted project to jsx

---
+++ exercises/part1/exercise1.12-1.14/anecdotes/src/components/NewAnecdote.jsx
@@ -0,0 +1,10 @@
+function NewAnecdote({nextAnecdote}) {
+  return (
+    <div className="new-anecdote">
+        <h2>Anecdotes of the day</h2>
+        <p>{nextAnecdote}</p>
+    </div>
+  )
+}
+
+export default NewAnecdote
